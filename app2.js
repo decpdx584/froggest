7d0ba09fb64fe6d4b40fda724eb776e093be1332
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-28 19:24:36 -0700

    [froggo] Attempt to draw frog

--- app2.js
+++ app2.js
@@ -1,15 +1,15 @@
-const canvas1 = document.getElementById("canvas1");
-const ctx1 = canvas1.getContext("2d");
+const canvas = document.getElementById("canvas1");
+const ctx1 = canvas.getContext("2d");
 canvas1.width = 600;
 canvas1.height = 900;
 
-const canva2 = document.getElementById("canvas2");
-const ctx2 = canvas2.getContext("2d");
-canva2.width = 600;
-canva2.height = 900;
+const canvas2 = document.getElementById("canvas2");
+const ctx2 = canvas.getContext("2d");
+canvas2.width = 600;
+canvas2.height = 900;
 
-const canvas3 = document.getElementById("canvas13");
-const ctx3 = canvas3.getContext("2d");
+const canvas3 = document.getElementById("canvas3");
+const ctx3 = canvas.getContext("2d");
 canvas3.width = 600;
 canvas3.height = 900;
 
@@ -24,7 +24,27 @@ const logsArray = [];
 
 
 
+class Froggo {
+    constructor() {
+        this.width = 50;
+        this.height = 50;
+        this.x = canvas.width / 2 - this.height / 2;
+        this.y = canvas.height - this.height - 40;
+        this.moving = false;
+    };
+    draw() {
+        ctx2.fillStyle = "blueViolet";
+        ctx2.fillRect(this.x, this.y, this.width, this.height);
 
+    };
+};
 
+const froggo = new Froggo();
 
+function move() {
+    ctx2.clearRect(0, 0, canvas.width, canvas.height);
+    froggo.draw();
+    requestAnimationFrame(move);
+};
 
+move();
