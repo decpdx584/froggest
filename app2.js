af6badee5f89f185d16de5e12fbb4e27157ffea3
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-07-28 18:46:07 -0700

    [variables] Add global variables

---
+++ app2.js
@@ -0,0 +1,30 @@
+const canvas1 = document.getElementById("canvas1");
+const ctx1 = canvas1.getContext("2d");
+canvas1.width = 600;
+canvas1.height = 900;
+
+const canva2 = document.getElementById("canvas2");
+const ctx2 = canvas2.getContext("2d");
+canva2.width = 600;
+canva2.height = 900;
+
+const canvas3 = document.getElementById("canvas13");
+const ctx3 = canvas3.getContext("2d");
+canvas3.width = 600;
+canvas3.height = 900;
+
+// Global Variables
+const grid = 75;
+let keys = [];
+let score = 0;
+let frame = 0;
+let gameSpeed = 1;
+
+const logsArray = [];
+
+
+
+
+
+
+
